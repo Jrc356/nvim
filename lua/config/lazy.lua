2d1316e6de18656a9b51c7de3e77d8d3c088dd4f
Jon Corbin <jon.corbin@prepared911.com>
2024-09-21 10:13:53 -0400

    so many updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -45,3 +45,18 @@ require("lazy").setup({
     },
   },
 })
+
+require("telescope").setup({
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+    },
+  },
+})
