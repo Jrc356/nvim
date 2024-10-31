84ae753913efb937f0656e4e04ecb0df6f028e3e
Jon Corbin <jon.corbin8@gmail.com>
2024-10-31 02:12:51 -0400

    lspconfig & debugger

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -7,10 +7,16 @@ return {
       "nvim-neotest/nvim-nio",
     },
     config = function()
-      local dap, dapui = require("dap"), require("dapui")
+      local dap = require("dap")
+      local dapui = require("dapui")
 
-      require("dapui").setup()
-      require("dap-go").setup()
+      -- dapui.setup()
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          dapui.setup()
+        end,
+      })
+      -- require("dap-go").setup()
 
       dap.listeners.before.attach.dapui_config = function()
         dapui.open()
@@ -25,14 +31,14 @@ return {
         dapui.close()
       end
 
-      vim.keymap.set("n", "<Leader>dt", ":DapUiToggle<CR>", {})
-      vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
-      vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-      vim.keymap.set("n", "<Leader>dr", ":lua require('dapui').open({reset = true})<CR>", {})
+      -- vim.keymap.set("n", "<Leader>dt", ":DapUiToggle<CR>", {})
+      -- vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+      -- vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+      -- vim.keymap.set("n", "<Leader>dr", ":lua require('dapui').open({reset = true})<CR>", {})
 
       vim.fn.sign_define(
         "DapBreakpoint",
-        { text = "⏺", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+        { text = "⏺️", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
       )
     end,
   },
