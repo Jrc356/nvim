4dec0f5df51545c16286d4cd24eacebb8995216d
Jon Corbin <jcorbin@launchdarkly.com>
2026-05-21 17:43:49 -0400

    update theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,19 @@
 return {
-  {
-    "navarasu/onedark.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "onedark",
-    },
-  },
+  -- {
+  --   "navarasu/onedark.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "onedark",
+  --   },
+  -- },
+  { "miikanissi/modus-themes.nvim", priority = 1000 },
 }
